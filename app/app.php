96255500af124ce81497f03e8eec28d9dff508cc
Jennifer Beem and Keith Evans <jennifer@>
2017-02-28 11:27:17 -0800

    add frontend for adding student and enrollment date

---
+++ app/app.php
@@ -0,0 +1,33 @@
+<?php
+
+    date_default_timezone_set("America/Los_Angeles");
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Student.php";
+    require_once __DIR__."/../src/Course.php";
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__."/../views"]);
+
+    $server = "mysql:host=localhost:8889;dbname=registrar";
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('home.html.twig');
+    });
+
+    $app->post('/student_add', function() use ($app) {
+        $name = $_POST['name'];
+        $enrollment_date = $_POST['enrollment-date'];
+        $new_student = new Student($name, $enrollment_date);
+        $new_student->save();
+        return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    });
+
+return $app;
+
+?>
